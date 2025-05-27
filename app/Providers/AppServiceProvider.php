a84c34b04c52845a1bbbfe42db016518da028499
natasyalabaso <natasyalabaso@gmail.com>
2025-05-27 17:02:30 +0800

    z

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,27 +22,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
         Gate::define("view-student", function (User $user) {
             if ($user->role === "admin" || $user->role === "guest") {
                 return true;
             }
             return false;
         });
-
         Gate::define("store-student", function (User $user) {
             if ($user->role === "admin") {
                 return true;
             }
             return false;
         });
-
         Gate::define("edit-student", function (User $user) {
             if ($user->role === "admin") {
                 return true;
             }
             return false;
         });
-
         Gate::define("destroy-student", function (User $user) {
             if ($user->role === "admin") {
                 return true;
